b30083208f361cdb0d5cb152d35727189745df12
marco.minetti <marco.minetti@novetica.org>
2015-03-22 00:16:01 +0100

    Disabled unused UI elements and fixed linkifier.

---
+++ front-end-node/components/ComponentsOverrides.js
@@ -0,0 +1,12 @@
+/*jshint browser:true, nonew:false*/
+/*global WebInspector:true*/
+
+var drawer_proto = WebInspector.Drawer.prototype;
+drawer_proto.oldWasShown = drawer_proto.wasShown;
+drawer_proto.wasShown = function()
+{
+    WebInspector.inspectorView.registerRequiredCSS('node/components/ComponentsOverrides.css');
+undefined
+    drawer_proto.wasShown = drawer_proto.oldWasShown;
+    drawer_proto.oldWasShown.call(this);
+}
---
+++ front-end-node/host/HostOverrides.js
@@ -0,0 +1,5 @@
+WebInspector.isWorkerFrontend = function()
+{
+    return true;
+    //return !!Runtime.queryParam("isSharedWorker");
+}
